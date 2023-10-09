83d2fa6fde2c249359552d8da698d7ce61274edc
Emmanue <kioxmanu7@gmail.com>
2023-10-09 17:36:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Send a fetch request to 'https://anapioficeandfire.com/api/books'
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(books => {
+      // Render book titles into the DOM by passing the JSON object to renderBooks
+      renderBooks(books);
+    })
+    .catch(error => {
+      console.error('Error fetching books:', error);
+    });
 }
 
 function renderBooks(books) {
